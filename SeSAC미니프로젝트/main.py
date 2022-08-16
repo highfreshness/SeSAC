4601f576a3aa744a250612baf524fbc8fb87f5d8
highfreshness <meteo0718@gmail.com>
2022-08-16 11:58:09 +0900

    html ignore exception

--- SeSAC미니프로젝트/main.py
+++ SeSAC미니프로젝트/main.py
@@ -1,12 +1,36 @@
-from typing import Union
 from fastapi import FastAPI
+# from fastapi.responses import FileResponse
+from typing import Union
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
+# @app.get('/')
+# def 작명():
+#     return 'Hello'
+
+# 파이썬 파일을 실행해도 uvicorn이 자동실행 되도록 하는 명령어
+if __name__ == '__main__':
+    uvicorn.run(app, host = '0.0.0.0', port = 8000)
+
+class Item(BaseModel):
+    name : str
+    price : float
+    is_offer : Union[bool, None] = None
+
 @app.get('/')
-async def read_root():
+def read_root():
     return {'Hello':'World'}
 
 @app.get('/items/{item_id}')
-async def read_item(item_id:int):
-    return {'item_id':item_id}
+def read_item(item_id:int, q:Union[str, None]=None):
+    return {'item_id':item_id, 'q':q}
+
+@app.put('/items/{item_id}')
+def update_item(item_id:int, item:Item):
+    return {'item_name':item.name, 'item_id':item_id, 'item_price':item.price}
+
+# @app.get('/fileresponse')
+# def data():
+#     return FileResponse('folium_test.html')
