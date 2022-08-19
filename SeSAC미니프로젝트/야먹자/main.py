c016cb25a2816a10e7e6846f510c8f9de4f6dbc6
highfreshness <meteo0718@gmail.com>
2022-08-19 09:14:05 +0900

    daily commit

--- SeSAC미니프로젝트/야먹자/main.py
+++ SeSAC미니프로젝트/야먹자/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-# from fastapi.responses import FileResponse
-from typing import Union
-from pydantic import BaseModel
+from fastapi.responses import FileResponse
 import uvicorn
 
 app = FastAPI()
@@ -12,25 +10,25 @@ app = FastAPI()
 
 # 파이썬 파일을 실행해도 uvicorn이 자동실행 되도록 하는 명령어
 if __name__ == '__main__':
-    uvicorn.run(app, host = '0.0.0.0', port = 8000)
+    uvicorn.run(app, host = '127.0.0.1', port = 8000, debug=True)
 
-class Item(BaseModel):
-    name : str
-    price : float
-    is_offer : Union[bool, None] = None
+# class Item(BaseModel):
+#     name : str
+#     price : float
+#     is_offer : Union[bool, None] = None
 
 @app.get('/')
-def read_root():
+def root():
     return {'Hello':'World'}
 
-@app.get('/items/{item_id}')
-def read_item(item_id:int, q:Union[str, None]=None):
-    return {'item_id':item_id, 'q':q}
+# @app.get('/items/{item_id}')
+# def read_item(item_id:int, q:Union[str, None]=None):
+#     return {'item_id':item_id, 'q':q}
 
-@app.put('/items/{item_id}')
-def update_item(item_id:int, item:Item):
-    return {'item_name':item.name, 'item_id':item_id, 'item_price':item.price}
+# @app.put('/items/{item_id}')
+# def update_item(item_id:int, item:Item):
+#     return {'item_name':item.name, 'item_id':item_id, 'item_price':item.price}
 
-# @app.get('/fileresponse')
-# def data():
-#     return FileResponse('folium_test.html')
+@app.get('/data')
+def data():
+    return FileResponse('folium_test.html')
